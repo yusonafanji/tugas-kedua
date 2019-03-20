6c17921c19d622a263c144e8ec11e76144e0f046
umiuks <umikhotimatus14@gmail.com>
2019-03-20 21:26:57 +0700

    tugas kedua

---
+++ app/src/main/java/com/afanji/ysr/myapplication/Firs_tFragment.java
@@ -0,0 +1,6 @@
+package com.afanji.ysr.myapplication;
+
+import android.app.Activity;
+
+public class Firs_tFragment extends Activity {
+}
---
+++ app/src/main/java/com/afanji/ysr/myapplication/fragment_second.java
@@ -0,0 +1,6 @@
+package com.afanji.ysr.myapplication;
+
+import android.app.Activity;
+
+public class fragment_second extends Activity {
+}
